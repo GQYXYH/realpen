normalization bug resolved

gym_brt/envs/reinforcementlearning_wrappers/vision_wrappers.py
@@ -22,7 +22,7 @@ to ensure safe closure of camera and qube!
 
 class VisionQubeBeginDownEnv(QubeSwingupEnv):
     def __init__(self, frequency=FREQUENCY, batch_size=2048, use_simulator=False, simulation_mode='ode',
-                 encoder_reset_steps=int(1e8), ):
+                 encoder_reset_steps=int(1e8), no_image_normalization=False):
         super().__init__(frequency, batch_size, use_simulator, simulation_mode, encoder_reset_steps, )
         self.observation_space = spaces.Box(low=0, high=255,
                                             shape=IMAGE_SHAPE, dtype=np.float32)
@@ -40,6 +40,7 @@ class VisionQubeBeginDownEnv(QubeSwingupEnv):
 
         self.use_simulator = use_simulator
         self.simulation_mode = simulation_mode
+        self.no_image_normalization = no_image_normalization
 
     def _get_state(self):
         if self.use_simulator:
@@ -51,7 +52,10 @@ class VisionQubeBeginDownEnv(QubeSwingupEnv):
                                  f"Valid ones are 'mujoco'")
         else:
             image = self.camera.get_image()
-            return self.preprocessor.preprocess_and_normalize_image(image)
+            if self.no_image_normalization:
+                return self.preprocessor.preprocess_image(image)
+            else:
+                return self.preprocessor.preprocess_and_normalize_image(image)
 
     def __enter__(self):
         self.camera.start_acquisition()